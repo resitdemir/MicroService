4afc081a295de9f0e6b581735e0714218b641c12
Reşit demir <resitdemir747@gmail.com>
2021-08-28 22:34:00 +0300

    Basket service added

---
+++ Basket/Dtos/BasketItemDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.Dtos
+{
+    public class BasketItemDto
+    {
+        public int Quantity { get; set; }
+        public int CourseId { get; set; }
+        public int CourseName { get; set; }
+        public int Price { get; set; }
+    }
+}
